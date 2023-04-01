f4ebfbdfe56afd6915f6d7df0ca537431bcbf256
Denis <den.zagrosh@gmail.com>
2023-04-02 00:39:58 +0300

    fix: error middleware

--- middlewares/error-middleware.js
+++ middlewares/error-middleware.js
@@ -1,6 +1,7 @@
 const ApiError = require('../exceptions/api-error');
 
-module.exports = function (err, req, res) {
+// eslint-disable-next-line no-unused-vars
+module.exports = function (err, req, res, next) {
 	console.log(err);
 	if (err instanceof ApiError) {
 		return res
